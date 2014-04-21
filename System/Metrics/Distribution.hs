3deedda2b025e2b4d94daec0e8a68bf4954f5f55
Johan Tibell <johan.tibell@gmail.com>
2014-04-21 15:49:26 +0200

    Clarify Distribution docs slightly

--- System/Metrics/Distribution.hs
+++ System/Metrics/Distribution.hs
@@ -5,8 +5,8 @@
 -- | This module defines a type for tracking statistics about a series
 -- of events. An event could be handling of a request and the value
 -- associated with the event -- the value you'd pass to 'add' -- could
--- be the amount of time spent serving the request. All operations are
--- thread safe.
+-- be the amount of time spent serving that request. All operations
+-- are thread safe.
 module System.Metrics.Distribution
     ( Distribution
     , new
@@ -57,7 +57,7 @@ emptyState = State
     , sMax        = -dblMax
     }
 
--- | Create a new event tracker.
+-- | Create a new distribution.
 new :: IO Distribution
 new = Distribution <$> newMVar emptyState
 
